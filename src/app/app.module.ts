47c7d70bfcc892a6c1e7ca894ca04d2333f85a61
crinoidms <crinoidms@gmail.com>
2021-09-24 14:11:09 +0800

    feat: separate nav and footer.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AbleIndexComponent } from './able-index/able-index.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AbleIndexComponent
+    AbleIndexComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
